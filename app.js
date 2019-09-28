e37928d0431d318af7150c745a48c2ab39430dfb
savoy1211 <ryanlee250@gmail.com>
2019-09-28 16:11:25 -0700

    setup to push to heroku

--- app.js
+++ app.js
@@ -44,20 +44,20 @@ const homeController = require('./controllers/home');
  * Create Express server.
  */
 const app = express();
-
+require('dotenv').config()
 /**
  * Connect to MongoDB.
  */
-//mongoose.set('useFindAndModify', false);
-//mongoose.set('useCreateIndex', true);
-//mongoose.set('useNewUrlParser', true);
-mongoose.set('useUnifiedTopology', false);
-mongoose.connect("mongodb://verve3349:Leeryan14*@ds349587.mlab.com:49587/jazzlib")
-//mongoose.connection.on('error', (err) => {
-//  console.error(err);
-//  console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
-//  process.exit();
-//});
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useNewUrlParser', true);
+//mongoose.set('useUnifiedTopology', false);
+mongoose.connect(process.env.MONGODB_URI ||"mongodb://verve3349:Leeryan14*@ds349587.mlab.com:49587/jazzlib")
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+  console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
+  process.exit();
+});
 
 /**
  * Express configuration.
@@ -75,16 +75,16 @@ app.use(sass({
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-//app.use(session({
-//  resave: true,
-//  saveUninitialized: true,
-//  secret: process.env.SESSION_SECRET,
-//  cookie: { maxAge: 1209600000 }, // two weeks in milliseconds
-//  store: new MongoStore({
-//    url: process.env.MONGODB_URI,
-//    autoReconnect: true,
-//  })
-//}));
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: process.env.SESSION_SECRET,
+  cookie: { maxAge: 1209600000 }, // two weeks in milliseconds
+  store: new MongoStore({
+    url: process.env.MONGODB_URI,
+    autoReconnect: true,
+  })
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
